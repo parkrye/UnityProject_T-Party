618e6f8e37f2829df8afefff41b02bee342bb85d
parkrye <parkrye@gmail.com>
2023-10-30 10:55:15 +0900

    초기 커밋.

---
+++ Assets/Scripts/Scene/TitleScene.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+
+public class TitleScene : BaseScene
+{
+    protected override IEnumerator LoadingRoutine()
+    {
+        throw new System.NotImplementedException();
+    }
+}
